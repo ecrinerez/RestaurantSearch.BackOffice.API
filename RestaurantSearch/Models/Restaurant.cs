aa39ab1fa76804e2aa9dd0880afadb785a3dee17
Ecrin Erez <ecrin.erez@gmail.com>
2026-02-18 20:50:23 +0300

    Adding model validation

--- RestaurantSearch/Models/Restaurant.cs
+++ RestaurantSearch/Models/Restaurant.cs
@@ -1,12 +1,21 @@
-﻿namespace RestaurantSearch.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantSearch.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = "";
+        [Required]
+        [MaxLength(100)]
         public string Address { get; set; } = "";
         public string PhoneNumber { get; set; } = "";
+        [Required]
         public string Cuisine { get; set; } = "";
+        [Range(0, 5)]
         public double Rating { get; set; }
     }
 }
